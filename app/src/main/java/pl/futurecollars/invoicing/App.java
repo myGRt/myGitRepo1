5712edf335115ca455e2a338d1675c68d2898609
arek <arek.giernatowski@gmail.com>
2021-07-31 20:29:10 +0200

    task4-

--- app/src/main/java/pl/futurecollars/invoicing/App.java
+++ app/src/main/java/pl/futurecollars/invoicing/App.java
@@ -1,37 +1,13 @@
 package pl.futurecollars.invoicing;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
-import pl.futurecollars.invoicing.db.Database;
-import pl.futurecollars.invoicing.db.memory.InMemoryDatabase;
-import pl.futurecollars.invoicing.model.Company;
-import pl.futurecollars.invoicing.model.Invoice;
-import pl.futurecollars.invoicing.model.InvoiceEntry;
-import pl.futurecollars.invoicing.model.Vat;
-import pl.futurecollars.invoicing.service.InvoiceService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
 
-        Database db = new InMemoryDatabase();
-        InvoiceService service = new InvoiceService(db);
-
-        Company buyer = new Company("5213861303", "ul. Bukowińska 24d/7 02-703 Warszawa, Polska", "iCode Trust Sp. z o.o");
-        Company seller = new Company("1234567890", "62-070 Dopiewo, Warszawska 23", "Arek Giernatowski Development");
-
-        List<InvoiceEntry> products = List.of(new InvoiceEntry("Programming course", BigDecimal.valueOf(10000),
-                BigDecimal.valueOf(2300), Vat.VAT_23));
-
-        Invoice invoice = new Invoice(LocalDate.now(), buyer, seller, products);
-
-        int id = service.save(invoice);
-
-        service.getById(id).ifPresent(System.out::println);
-
-        System.out.println(service.getAll());
-
-        service.delete(id);
+        SpringApplication.run(App.class, args);
     }
 }
