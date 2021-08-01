003e262e4b0c11eae2fd3a7a6d530e8b2cfb76c2
myGRt <88236287+myGRt@users.noreply.github.com>
2021-08-01 20:39:21 +0200

    task8- (#8)
    Co-authored-by: arek <arek.giernatowski@gmail.com>

--- app/src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
+++ app/src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
@@ -3,20 +3,14 @@ package pl.futurecollars.invoicing.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.futurecollars.invoicing.model.Invoice;
 import pl.futurecollars.invoicing.service.InvoiceService;
 
 @RestController
-@RequestMapping("invoices")
-public class InvoiceController {
+public class InvoiceController implements InvoiceApi {
 
     private final InvoiceService invoiceService;
 
@@ -25,31 +19,31 @@ public class InvoiceController {
         this.invoiceService = invoiceService;
     }
 
-    @GetMapping(produces = {"application/json;charset=UTF-8"})
+    @Override
     public List<Invoice> getAllInvoices() {
         return invoiceService.getAll();
     }
 
-    @PostMapping
+    @Override
     public int addInvoice(@RequestBody Invoice invoice) {
         return invoiceService.save(invoice);
     }
 
-    @GetMapping(value = "/{id}", produces = {"application/json;charset=UTF-8"})
+    @Override
     public ResponseEntity<Invoice> getById(@PathVariable int id) {
         return invoiceService.getById(id)
                 .map(invoice -> ResponseEntity.ok().body(invoice))
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @DeleteMapping("/{id}")
+    @Override
     public ResponseEntity<?> deleteById(@PathVariable int id) {
         return invoiceService.delete(id)
                 .map(invoice -> ResponseEntity.noContent().build())
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @PutMapping("/{id}")
+    @Override
     public ResponseEntity<?> updateById(@PathVariable int id, @RequestBody Invoice updatedInvoice) {
         return invoiceService.update(id, updatedInvoice)
                 .map(invoice -> ResponseEntity.noContent().build())
