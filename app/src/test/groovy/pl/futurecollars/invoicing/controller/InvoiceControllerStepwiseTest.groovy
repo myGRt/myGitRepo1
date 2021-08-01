e32d2c49b34d6a316fd72133ba4161a950dc84a9
arek <arek.giernatowski@gmail.com>
2021-08-01 23:25:22 +0200

    task9-update4

--- app/src/test/groovy/pl/futurecollars/invoicing/controller/InvoiceControllerStepwiseTest.groovy
+++ app/src/test/groovy/pl/futurecollars/invoicing/controller/InvoiceControllerStepwiseTest.groovy
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @AutoConfigureMockMvc
 @SpringBootTest
-@Stepwise
+//@Stepwise
 class InvoiceControllerStepwiseTest extends Specification {
 
     @Autowired
