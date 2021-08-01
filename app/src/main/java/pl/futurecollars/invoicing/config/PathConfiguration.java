3c6482d9a5028a8dfd6defed39c56f1d692973d5
myGRt <88236287+myGRt@users.noreply.github.com>
2021-08-01 20:16:28 +0200

    task7- (#7)
    Co-authored-by: arek <arek.giernatowski@gmail.com>

--- app/src/main/java/pl/futurecollars/invoicing/config/PathConfiguration.java
+++
@@ -1,8 +0,0 @@
-package pl.futurecollars.invoicing.config;
-
-public class PathConfiguration {
-
-    public static final String INVOICES_PATH = "invoices.txt";
-    public static final String ID_PATH = "id.txt";
-    public static final String DATABASE_PATH = "db";
-}
