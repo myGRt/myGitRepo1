ee08bcd84d32a5d7a650c74fb352779202e99b22
arek <arek.giernatowski@gmail.com>
2021-08-02 08:20:51 +0200

    task9-update17

--- app/src/test/groovy/pl/futurecollars/invoicing/controller/InvoiceControllerStepwiseTest.groovy
+++ app/src/test/groovy/pl/futurecollars/invoicing/controller/InvoiceControllerStepwiseTest.groovy
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
+import pl.futurecollars.invoicing.model.Invoice
+import pl.futurecollars.invoicing.service.JsonService
 import spock.lang.Specification
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -11,10 +13,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 
 @AutoConfigureMockMvc
 @SpringBootTest
-class InvoiceControllerStepwiseTest extends ControllerTest {
+class InvoiceControllerStepwiseTest extends Specification {
 
     @Autowired
     private MockMvc mockMvc
+
+    @Autowired
+    JsonService jsonService
 
     def "empty array is returned when no invoices were added"() {
 
@@ -35,6 +40,21 @@ class InvoiceControllerStepwiseTest extends ControllerTest {
                 .andReturn()
                 .response
                 .contentAsString == "[]"
+    }
+
+    def "empty array is returned when no invoices were added2"() {
+        expect:
+        getAllInvoices() == []
+    }
+
+    List<Invoice> getAllInvoices() {
+        def response = mockMvc.perform(get("/invoices"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .response
+                .contentAsString
+
+        jsonService.stringToObject(response, Invoice[])
     }
 
 
