04d30aaeb9527ff3b3a5defe399fffe6f4efd3e2
arek <arek.giernatowski@gmail.com>
2021-09-03 08:49:51 +0200

    task17-bootsrap-and-add-company

--- app/src/main/java/pl/futurecollars/invoicing/controller/invoice/InvoiceApi.java
+++ app/src/main/java/pl/futurecollars/invoicing/controller/invoice/InvoiceApi.java
@@ -4,15 +4,10 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.futurecollars.invoicing.model.Invoice;
 
+@CrossOrigin
 @RequestMapping(value = "invoices", produces = {"application/json;charset=UTF-8"})
 @Api(tags = {"invoices"})
 public interface InvoiceApi {
